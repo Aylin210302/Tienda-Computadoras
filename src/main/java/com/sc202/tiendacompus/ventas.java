a15c76cbbe9ae09ffc5288327b2941eba0d7d99a
Aylin210302 <aylinorozco856@gmail.com>
2020-07-30 23:24:35 -0600

    nuevo cambio

--- src/main/java/com/sc202/tiendacompus/ventas.java
+++ src/main/java/com/sc202/tiendacompus/ventas.java
@@ -2,20 +2,20 @@
 package com.sc202.tiendacompus;
 public class ventas {
     public String productos;
-    public int presupuesto;
-    public int precio;
     public String marca;
+    public int precio;
     public String capacidad;
     public String proposito;
     public String tipo;
+    public String accesorio;
 
-public ventas(){
+public ventas(String productos, String marca, String capacidad, String proposito, String tipo, String accesorio, int precio){
    this.productos = productos;
-   this.presupuesto = presupuesto;
-   this.precio = precio;
    this.marca = marca;
    this.capacidad = capacidad;
    this.proposito = proposito;
    this.tipo = tipo;
+   this.accesorio = accesorio;
+   this.precio = precio;
 }
 }
