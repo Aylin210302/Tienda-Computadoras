108e858cc906f17d8bf725935006f1a0f9d7e1ae
Aylin210302 <aylinorozco856@gmail.com>
2020-08-01 07:06:22 -0600

    casi

--- src/main/java/com/sc202/tiendacompus/Regalias.java
+++ src/main/java/com/sc202/tiendacompus/Regalias.java
@@ -1,5 +1,28 @@
 
 package com.sc202.tiendacompus;
 public class Regalias {
-    
+    public String rifa;
+    public String descuento;
+
+    public String getRifa() {
+        return rifa;
+    }
+
+    public void setRifa(String rifa) {
+        this.rifa = rifa;
+    }
+
+    public String getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(String descuento) {
+        
+        
+        this.descuento = descuento;
+    }
+public Regalias(String rifa, String descuento){
+    this.descuento = descuento;
+    this.rifa = rifa;
+}
 }
