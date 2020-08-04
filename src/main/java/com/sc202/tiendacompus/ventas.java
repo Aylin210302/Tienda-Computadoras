9dd1ec29f324be2466434c9f399bfa245e76a79c
Aylin210302 <aylinorozco856@gmail.com>
2020-08-04 06:32:19 -0600

    listo

--- src/main/java/com/sc202/tiendacompus/ventas.java
+++ src/main/java/com/sc202/tiendacompus/ventas.java
@@ -8,12 +8,12 @@ public void compras(){
   int cantidad = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos productos desea comprar"));
         ventas[] arreglo;
         arreglo = new ventas[cantidad];
-        
+        Regalias miregalo = new Regalias();
         JOptionPane.showMessageDialog(null, "Por favor seleccione una opcion");
         
         do{
             for(int i = 0; i < arreglo.length; i++){
-            String opcion = JOptionPane.showInputDialog(null, "a. LAPTOPS" + "b. Accesorios" + "c. Arma la tuya" + "d. He terminado de comprar");
+            String opcion = JOptionPane.showInputDialog(null, "a. LAPTOPS" + "b. Accesorios" + "c. Aescoger" + "d. He terminado de comprar");
             
             
             switch(opcion){
@@ -23,50 +23,52 @@ public void compras(){
                         String proposito = JOptionPane.showInputDialog("¿Para que ocupas la computadora? trabajo o gaming");
                         JOptionPane.showMessageDialog(null, "Estamos buscando una buena opcion para ti, esto tomara unos minutos");
                         JOptionPane.showMessageDialog(null, "Hemos encontrado varias opciones para ti");
-                        if(capacidad == "media" || proposito == "trabajo"){
+                        if(capacidad == "media" && proposito == "trabajo"){
                             String opciones = JOptionPane.showInputDialog("Tenemos 3 opciones " + " a. HP de 4GB en 359.000" + " b. Lenovo de 8GB en 384200" + " c. Portatil DELL de 4GB en 378000 ");
                             switch(opciones){
                                 case "a":
                                     String marca = "HP";
                                     String RAM = "4GB";
                                     int precio = 359000;
-                                    JOptionPane.showMessageDialog(null,marca + RAM + precio);
+                                    JOptionPane.showMessageDialog(null,marca + RAM + " Sale en un valor de: " + precio);
                                     break;
                                 case "b":
                                     String modelo = "HP";
                                     String RAM1 = "8GB";
                                     int monto = 384200;
-                                    JOptionPane.showMessageDialog(null, modelo + RAM1 + monto);
+                                    JOptionPane.showMessageDialog(null, modelo + RAM1 + " Sale en un valor de: " + monto);
                                     break;
                                 case "c":
                                     String tipo = "DELL";
                                     String RAM2 = "4GB";
                                     int valor = 378000;
-                                    JOptionPane.showMessageDialog(null, tipo + RAM2 + valor);
+                                    JOptionPane.showMessageDialog(null, tipo + RAM2 + " Sale en un valor de: " + valor);
                                     break;
                                     
                             }//fin de switch
-                        }else if(capacidad == "alta" || proposito == "gaming"){
+                        }else if(capacidad == "alta" && proposito == "gaming"){
                             String variedad = JOptionPane.showInputDialog("Tenemos 3 opciones que te pueden ayudar " + " a. XPG XENIA de 32GB en 1101750 colones " + " b. Lenovo de 12GB en 624890 colones " + " c. Levono de 8GB en 706250 colones");
                             switch(variedad){
                                 case "a":
                                   String modelo1 = "XPG XENIA";
                                   String memoria1 = "32GB";
                                   int precio1 = 1101750;
-                                  JOptionPane.showMessageDialog(null, modelo1 + memoria1 + precio1);
+                                  JOptionPane.showMessageDialog(null, modelo1 + memoria1 + " En total queda en: " + precio1);
                                   
                                   break;
                                 case "b":
                                   String modelo2 = "Lenovo";
                                   String memoria2 = "12GB";
                                   int precio2 = 624890;
-                                  JOptionPane.showMessageDialog(null, modelo2 + memoria2 + precio2);
+                                  JOptionPane.showMessageDialog(null, modelo2 + memoria2 + " En total queda en: " + precio2);
+                                  miregalo.regalia1();
                                   break;
                                 case "c":
                                   String modelo3 = "Lenovo";
                                   String memoria3 = "8GB";
                                   int precio3 = 706250;
-                                  JOptionPane.showMessageDialog(null, modelo3 + memoria3 + precio3);
+                                  JOptionPane.showMessageDialog(null, modelo3 + memoria3 + " En total sale en: " + precio3);
+                                  miregalo.regalia1();
                                   break;
                             }//fin del switch
                         }//fin del else
@@ -111,8 +113,8 @@ public void compras(){
                     }//fin del switch accesorio
                     break;
                 case "c":
-                   String armala = JOptionPane.showInputDialog("¿Que tipo andas buscando? " + " a. Uso domestico " + " b. Gama Media " + " c. Gama alta" );
-                switch(armala){
+                   String Aescoger = JOptionPane.showInputDialog("¿Que tipo andas buscando? " + " a. Uso domestico " + " b. Gama Media " + " c. Gama alta" );
+                switch(Aescoger){
                     case "a":
                         String monitor1 = "Acer 21.5";
                         int preMon1 = 80000;
@@ -123,6 +125,7 @@ public void compras(){
                         String RAM1 = "SODIMM de 4GB";
                         int preRAM1 = 28476;
                         int total1 = (preMon1 + preAlm1 + preRAM1);
+                        JOptionPane.showMessageDialog(null, "Los componentes son: " + monitor1 + almacenamiento1 + procesador1 + RAM1);
                         JOptionPane.showMessageDialog(null,"En el primer caso tendria un total de: " +  total1);
                         break;
                     case "b":
@@ -135,6 +138,8 @@ public void compras(){
                         String discoduro2 = "500GB";
                         int preDisc2 = 39500;
                         int total2 = prePro2 + preRAM2 + preAlm2 + preDisc2;
+                        JOptionPane.showMessageDialog(null, "Los componentes son: " + procesador2 + RAM2 + almacenamiento2 + discoduro2);
+                        miregalo.regalia2();
                         JOptionPane.showMessageDialog(null, "Esta opcion tiene un costo de: " + total2);
                         break;
                     case "c":
@@ -147,13 +152,26 @@ public void compras(){
                         String discoduro3 = "1TB";
                         int preDisc3 = 46900;
                         int total3 = prePro3 + preRAM3 + preAlm3 + preDisc3;
+                        JOptionPane.showMessageDialog(null, "Los componentes son: " + procesador3 + RAM3 + almacenamiento3 + discoduro3);
                         JOptionPane.showMessageDialog(null, "Esta ultima opcion mas avanzada tiene un costo de; " + total3);
-                        
+                        int gamaPlus = Integer.parseInt(JOptionPane.showInputDialog("Para que enfoque seria tu PC: " + " 1. Gaming " + " 2. Edicion o animacion " + " 3. Animacion 3D"));
+                        switch(gamaPlus){
+                            case 1:
+                            miregalo.regalia3();
+                            break;
+                            case 2:
+                            miregalo.regalia4();
+                            break;
+                            case 3:
+                            miregalo.regalia5();
+                            break;
+                                
+                        }
                         break;
                         
                         
                         
-                }//fin del switch armala
+                }//fin del switch Aescoger
                 
                 break; 
         }//fin del swicth opcion
